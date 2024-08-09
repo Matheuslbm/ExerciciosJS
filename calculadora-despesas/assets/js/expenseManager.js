62cacb8b6061ad1eba4c5007fb13c54f7f5342ad
Matheus Lima <matheusslimabm@gmail.com>
2024-08-09 19:14:50 -0300

    expenseManager
    gerencia a lista de despesas e a persistencia dos dados

--- calculadora-despesas/assets/js/expenseManager.js
+++ calculadora-despesas/assets/js/expenseManager.js
@@ -1,3 +1,47 @@
 //Armazena e gerencia a lista de despesas
 // Metodos para adicionar, remover e editar despesas.
 // Metodos para calcular o total geral e o total por categoria.
+
+import { Expense } from "./expense.js";
+
+export class ExpenseManager {
+    constructor() {
+        this.expenses = this.loadExpenses();
+    }
+
+    addExpense(name, amount, category) {
+        const expense = new Expense(name, amount, category);
+        this.expenses.push(expense);
+        this.saveExpenses();
+    }
+
+    removeExpense(id) {
+        this.expenses = this.expenses.filter(expense => expense.id !== id);
+        this.saveExpenses();
+    }
+
+    loadExpenses() {
+        const expenses = localStorage.getItem('expenses');
+        return expenses ? JSON.parse(expenses) : [];
+    }
+
+    saveExpenses() {
+        localStorage.setItem('expenses', JSON.stringify(this.expenses));
+    }
+
+    getTotalByCategory() {
+        const totals = {};
+        this.expenses.forEach(expense => {
+            if(totals[expense.category]) {
+                total[expense.category] += expense.amount;
+            } else {
+                totals[expense.category] = expense.amount;
+            }
+        });
+        return totals;
+    }
+
+    getExpenses() {
+        return this.expenses;
+    }
+}
