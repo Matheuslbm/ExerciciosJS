35b02fc351656506e131d5e07411f67930ae16f0
Matheus Lima <matheusslimabm@gmail.com>
2024-08-09 18:18:22 -0300

    expense.js
    criação da classe que representa uma despesa individual

--- calculadora-despesas/assets/js/expense.js
+++ calculadora-despesas/assets/js/expense.js
@@ -1,1 +1,11 @@
 // Responsavel por representar uma despesa com propriedades como nome, valor e categoria
+// despesa individual
+
+export class Expense {
+    constructor(name, a,ount, category){
+        this.name = name,
+        this.amount = parseFloat(amount);
+        this.category = category;
+        this.id = Date.now();
+    }
+}
