24afc69528f447867050856e66c1fa62b9815912
Matheus Lima <matheusslimabm@gmail.com>
2024-08-19 19:54:45 -0300

    Add funcionalidade
    agora ela diz o quanto voce precisa comer para seu objetivo

--- calculadoraTMBeGET/assets/js/UI.js
+++ calculadoraTMBeGET/assets/js/UI.js
@@ -4,7 +4,23 @@ export class UI {
     constructor() {
         this.form = document.getElementById('tmb-get-form');
         this.resultsElement = document.getElementById('results')
+
+        this.calorieSuggestionElement = document.getElementById('calorie-suggestion');
+        this.goalElement = document.getElementById('goal');
+        this.weightAdjustmentElement = document.getElementById('weight-adjustment');
+
+        this.goalElement.addEventListener('change', () => this.handleGoalChange());
+
         this.form.addEventListener('submit', (e) => this.handleSubmit(e));
+    }
+
+    handleGoalChange() {
+        const goal = this.goalElement.value;
+        if(goal === 'manter') {
+            this.weightAdjustmentElement.disabled = true;
+        } else {
+            this.weightAdjustmentElement.disabled = false;
+        }
     }
 
     handleSubmit(e) {
@@ -14,12 +30,28 @@ export class UI {
         const height = parseFloat(document.getElementById('height').value);
         const gender = document.getElementById('gender').value;
         const activityLevel = parseFloat(document.getElementById('activity-level').value);
+        const goal = this.goalElement.value;
+        const weightAdjustment = parseFloat(this.weightAdjustmentElement.value);
 
         const get = GETCalculator.calculate(age, weight, height, gender, activityLevel);
-        this.showResults(get)
+        this.showResults(get, goal, weightAdjustment);
     }
 
-    showResults(get) {
+    showResults(get, goal, weightAdjustment) {
         this.resultsElement.innerHTML = `Seu GET é de <strong>${get.toFixed(2)}</strong> calorias por dia.`
+
+        let calorieIntake;
+
+        if(goal === 'manter') {
+            calorieIntake = get;
+        } else if (goal === 'emagrecer'){
+            const calorieDeficit = weightAdjustment * 7700 / 7;
+            calorieIntake = get - calorieDeficit;
+        } else if ( goal === 'ganhar') {
+            const calorieSurplus = weightAdjustment * 7700 / 7;
+            calorieIntake = get + calorieSurplus;
+        }
+
+        this.calorieSuggestionElement.innerHTML = `Para ${goal}, você deve consumir ${calorieIntake.toFixed(2)} calorias por dia. `
     }
 }
